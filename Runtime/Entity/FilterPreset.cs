2cbd21dc16845b9c9bdccacc987a06b1d00bfeee
詹易衡 <jerryee@redcandlegames.com>
2023-03-17 12:43:44 +0800

    rm testing scripts

--- Runtime/Entity/FilterPreset.cs
+++ Runtime/Entity/FilterPreset.cs
@@ -4,15 +4,19 @@ using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
-
+namespace RCGMaker.Core
+{
 //給編輯Prefab時，能快速的過濾掉不需要的Component
 //專注編輯目前關心的Component
-[CreateAssetMenu(fileName = "FilterPreset", menuName = "RCG/FilterPreset")]
+    [CreateAssetMenu(fileName = "FilterPreset", menuName = "RCG/ScriptableObject/FilterPreset")]
 #if UNITY_EDITOR
-public class FilterPreset : SerializedScriptableObject
+    public class FilterPreset : SerializedScriptableObject
 #else
 public class FilterPreset : ScriptableObject
 #endif
-{
-    public Type[] allowTypes;
+    {
+        public Type ModuleEntityType;
+        public Type[] allowTypes;
+    }
+    
 }
