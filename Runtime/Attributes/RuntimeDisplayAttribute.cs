cc91e8de7c8819e9d8453484539f5c5e88fcb139
詹易衡 <jerryee@redcandlegames.com>
2023-05-07 19:18:49 +0800

    RuntimeDisplay Processor

--- Runtime/Attributes/RuntimeDisplayAttribute.cs
+++ Runtime/Attributes/RuntimeDisplayAttribute.cs
@@ -1,13 +1,18 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Reflection;
 using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 namespace RCGMaker.Core.Attributes
 {
     [IncludeMyAttributes]
-    [HideInPlayMode] //NOTE: 沒用，還是會call property, 但是不會顯示
-    [ShowInInspector, ReadOnly]
+    // [HideInPlayMode] //NOTE: 沒用，還是會call property, 但是不會顯示
+    [ReadOnly]
     [Conditional("UNITY_EDITOR")]
     public class RuntimeDisplayAttribute : Attribute
     {
@@ -24,5 +29,41 @@ namespace RCGMaker.Core.Attributes
     [Conditional("UNITY_EDITOR")]
     public class ConfigAttribute : Attribute
     {
+    }
+
+    public class MyProcessedClassAttributeProcessor : OdinAttributeProcessor
+    {
+        // public override void ProcessSelfAttributes(InspectorProperty property, List<Attribute> attributes)
+        // {
+        //     // attributes.Add(new InfoBoxAttribute("Dynamically added attributes!"));
+        //     // attributes.Add(new InlinePropertyAttribute());
+        //     Debug.Log(property.Name);
+        //     var memberInfo = property.Info.GetMemberInfo();
+        //     // Debug.Log(memberInfo);
+        //     var runtimeDisplayAttribute = property.Info.GetMemberInfo().GetAttribute<RuntimeDisplayAttribute>();
+        //     // Debug.Log(runtimeDisplayAttribute);
+        //     if (runtimeDisplayAttribute != null)
+        //     {
+        //         if (Application.isPlaying)
+        //         {
+        //             attributes.Add(new ShowInInspectorAttribute());
+        //         }
+        //     }
+        // }
+
+        public override void ProcessChildMemberAttributes(
+            InspectorProperty parentProperty,
+            MemberInfo member,
+            List<Attribute> attributes)
+        {
+            var runtimeDisplayAttribute = member.GetAttribute<RuntimeDisplayAttribute>();
+            if (runtimeDisplayAttribute != null)
+            {
+                if (Application.isPlaying)
+                {
+                    attributes.Add(new ShowInInspectorAttribute());
+                }
+            }
+        }
     }
 }
