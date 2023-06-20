f553ac79fedf3de6e2ed2de33d5ee3807136fb8a
詹易衡 <jerryee@redcandlegames.com>
2023-06-20 18:30:08 +0800

    line gizmo

---
+++ Runtime/Gizmo/LineGizmoNode.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+namespace Gizmo
+{
+    public class LineGizmoNode : MonoBehaviour
+    {
+        public Vector3 offset;
+        public Color color = Color.red;
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = color;
+            Gizmos.DrawLine(transform.position, transform.position + offset);
+        }
+    }
+}
