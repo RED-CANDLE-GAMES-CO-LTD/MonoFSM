48bb9f5092e829c6f7c6ed662a8cbb25c4b8e4c5
詹易衡 <jerryee@redcandlegames.com>
2023-07-24 17:58:56 +0800

    edit hide flag in monoscript menuitem context

---
+++ Runtime/Extension/ContextMenuItemForMonoScript.cs
@@ -0,0 +1,45 @@
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace RCGMaker.Core
+{
+    public static class ContextMenuItemForMonoScript
+    {
+#if UNITY_EDITOR
+
+        [MenuItem("CONTEXT/MonoBehaviour/Filter Logs for me")]
+        public static void FindLog(MenuCommand command)
+        {
+            var owner = command.context;
+            var id = owner.GetInstanceID();
+            EditorGUIUtility.systemCopyBuffer = id.ToString();
+
+            var assembly = Assembly.GetAssembly(typeof(SceneView));
+            var consoleWindowType = assembly.GetType("UnityEditor.ConsoleWindow");
+            var consoleWindow = EditorWindow.GetWindow(consoleWindowType);
+            var setFilterMethod =
+                consoleWindowType.GetMethod("SetFilter", BindingFlags.Instance | BindingFlags.NonPublic);
+            setFilterMethod.Invoke(consoleWindow, new object[] { id.ToString() });
+        }
+
+        [MenuItem("CONTEXT/Component/HideFlag/None")]
+        public static void ResetHideFlag(MenuCommand command)
+        {
+            var owner = command.context as Component;
+            if (owner != null) owner.hideFlags = HideFlags.None;
+            else
+                Debug.LogError("Can't find Component");
+        }
+
+        [MenuItem("CONTEXT/Component/HideFlag/Lock")]
+        public static void LockTransformHideFlag(MenuCommand command)
+        {
+            var owner = command.context as Component;
+            if (owner != null) owner.hideFlags = HideFlags.NotEditable;
+            else
+                Debug.LogError("Can't find Component");
+        }
+#endif
+    }
+}
