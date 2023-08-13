0f445cef220a1114585cc463a5ddf6aece2759b0
chao3 <aaefiikmnnnr@gmail.com>
2023-08-13 11:10:31 +0800

    Replace Resources.LoadAll with Resources.Load to reduce allocation

--- RCGMaker/Runtime/FSM/3_FlagData/Config/ScriptableObjectSingleton.cs
+++ RCGMaker/Runtime/FSM/3_FlagData/Config/ScriptableObjectSingleton.cs
@@ -1,9 +1,7 @@
-using RCGMaker.Core;
-using RCGMaker.Core.Attributes;
-using Sirenix.OdinInspector;
-using UnityEditor;
+using System.ComponentModel;
 using UnityEngine;
-using UnityEngine.UI;
+using Sirenix.OdinInspector;
+using RCGMaker.Core;
 
 //大部分的Static Config用這個, 可以依照testMode來選擇不同組config
 public class ScriptableObjectConfig<T> : ScriptableObjectSingleton<T> where T : ScriptableObject
@@ -15,39 +13,35 @@ public class ScriptableObjectConfig<T> : ScriptableObjectSingleton<T> where T : 
 //Singleton config，要放到Resources的Config資料夾裡
 public class ScriptableObjectSingleton<T> : ScriptableObject, ISelfValidator where T : ScriptableObject
 {
+    public void Validate(SelfValidationResult result) 
+        => this.AssetInFolderValidate("Resources/Configs", result);
 
-    public void Validate(SelfValidationResult result)
-    {
-        this.AssetInFolderValidate("Resources/Configs", result);
-    }
-    
     private static T s_Instance;
+    private static bool s_isLoaded;
     
     public static T Instance
     {
         get
         {
-            if (s_Instance == null)
+            if (!s_isLoaded)
             {
-// #if UNITY_EDITOR
-                // var findAssets = AssetDatabase.FindAssets($"t:{typeof(T).Name}");
-                // if (findAssets == null || findAssets.Length == 0)
-                //     Debug.LogError($"Please create ScriptableObject typeof {typeof(T)} first...");
-                // else if (findAssets.Length > 1)
-                //     Debug.LogError($"ScriptableObject typeof {typeof(T)} exist multiple，please check they...");
-                // else
-                //     s_Instance = AssetDatabase.LoadAssetAtPath<T>(AssetDatabase.GUIDToAssetPath(findAssets[0]));
-
-                var assets = Resources.LoadAll<T>("Configs");
-                s_Instance = assets[0];
-// #else
-                //TODO: 這裡要改成從Resources讀取
-// if(TestModeGameFlag.Instance.test
-                // s_Instance = Resources.Load<T>(typeof(T).Name);
-// #endif
+                s_Instance = Resources.Load<T>(GetPath());
+                s_isLoaded = true;
             }
 
             return s_Instance;
         }
     }
+
+    private static string GetPath() 
+        => typeof(T).Name switch
+        {
+            "RCGCoreConfig" => "Configs/Build_RCGCoreConfig",
+            "DropItemCollection" => "Configs/Drop Collection Config",
+            "GameConfig" => "Configs/GameConfig Production",
+            "MonsterGlobalConfig" => "Configs/MonsterGlobalConfig",
+            "SceneTable" => "Configs/SceneTable",
+            "TestModeGameFlag" => "Configs/TestModeGameFlag",
+            _ => throw new InvalidEnumArgumentException()
+        };
 }
