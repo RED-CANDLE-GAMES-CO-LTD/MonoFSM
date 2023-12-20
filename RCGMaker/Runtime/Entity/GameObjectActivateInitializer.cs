c1b2bc60f422e03b12f8c48c06d6b237782a2229
詹易衡 <jerryee@redcandlegames.com>
2023-12-20 18:04:51 +0800

    readonly

--- RCGMaker/Runtime/Entity/GameObjectActivateInitializer.cs
+++ RCGMaker/Runtime/Entity/GameObjectActivateInitializer.cs
@@ -4,24 +4,32 @@ using RCGMaker.Core;
 using UnityEngine;
 
 //存檔時，強迫設定這個物件的active狀態
-public class GameObjectInitialActivator : MonoBehaviour, ILevelConfig, ISceneSavingCallbackReceiver,
+
+public class ReadOnlyActivator : MonoBehaviour
+{
+    //只能被Editor改，不能被code改
+    [SerializeField] private bool isActive = false;
+    protected bool IsActive => isActive;
+}
+
+public class GameObjectInitialActivator : ReadOnlyActivator, ILevelConfig, ISceneSavingCallbackReceiver,
     IPrefabSavingCallbackReceiver
 {
-    public bool isActive = false;
+    // public bool isActive = false;
 
     public void SetLevelConfig() //這個不好用... interface也不能serialize, 後面撈太晚了？
     {
-        gameObject.SetActive(isActive);
+        gameObject.SetActive(IsActive);
     }
 
 
     public void OnBeforeSceneSave()
     {
-        gameObject.SetActive(isActive);
+        gameObject.SetActive(IsActive);
     }
 
     public void OnBeforePrefabSave()
     {
-        gameObject.SetActive(isActive);
+        gameObject.SetActive(IsActive);
     }
 }
