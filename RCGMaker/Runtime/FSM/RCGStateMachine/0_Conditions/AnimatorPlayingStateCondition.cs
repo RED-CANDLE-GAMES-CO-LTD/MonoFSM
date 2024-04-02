7da279b9a0141840de482e44cf001939ecc73a9f
詹易衡 <jerryee@redcandlegames.com>
2024-04-02 13:14:59 +0800

    animator play condition

---
+++ RCGMaker/Runtime/FSM/RCGStateMachine/0_Conditions/AnimatorPlayingStateCondition.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using RCGMaker.Core.Attributes;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace RCGMaker.Core
+{
+    public interface IAnimatorGetter
+    {
+        Animator GetAnimator();
+    }
+
+    public class AnimatorPlayingStateCondition : AbstractConditionComp
+    {
+        //拿動畫上的所有state name
+#if UNITY_EDITOR
+        public IEnumerable<string> GetAnimatorStateNames()
+        {
+            return AnimatorHelpler.GetAnimatorStateNames(target, layerIndex);
+        }
+#endif
+        [PreviewInInspector] [AutoParent] private IAnimatorGetter animatorProvider;
+        private Animator _animator => animatorProvider?.GetAnimator();
+        public Animator target;
+
+        [ValueDropdown(nameof(GetAnimatorStateNames), IsUniqueList = true, NumberOfItemsBeforeEnablingSearch = 3)]
+        public string stateName;
+
+        public int layerIndex = 0;
+        protected override bool isValid => _animator.GetCurrentAnimatorStateInfo(layerIndex).IsName(stateName);
+    }
+}
