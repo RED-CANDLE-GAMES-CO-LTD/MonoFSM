1c32468a3fdb4274930f1e85b4cf2b117d62e6c3
huw12313212 <huw12313212@gmail.com>
2024-04-11 12:07:18 +0800

    Revert "[fix]有時候覺得撿屍特效在某些怪物身上特別小？"
    This reverts commit 1497c1041ca83666308a81269f2d80d488cb246e.

--- RCGMaker/Runtime/PoolObject/PoolObject.cs
+++ RCGMaker/Runtime/PoolObject/PoolObject.cs
@@ -278,13 +278,11 @@ public class PoolObject : MonoBehaviour, ILevelAwake, ILevelResetPrepare
     public void PoolObjectResetAndStart() //只有收進去pool的才需要這個
     {
         // this.Break();
-
-        this.LevelResetPrepareRuntimeData();
         CheckList();
 
         //FIXME: animator可能還關著喔
         //因為關monsterCore，才發生這件悲劇
-       // this.ResetAnim();
+        this.ResetAnim();
 
         this.Log("[PoolObjectResetAndStart]", gameObject);
 
