90dcdfb8007e4daf754732e23fb307591f42c3be
huw12313212 <huw12313212@gmail.com>
2024-04-16 16:36:48 +0800

    Update Flag

--- RCGMaker/Runtime/FSM/3_FlagData/FlagTypeScriptable.cs
+++ RCGMaker/Runtime/FSM/3_FlagData/FlagTypeScriptable.cs
@@ -5,6 +5,19 @@ using UnityEngine;
 //綁定類別和動畫
 public class FlagTypeScriptable : ScriptableObject
 {
+ 
+    //Header("中寶箱都先統計進大寶箱")
+    public FlagTypeScriptable OverrideBy;
+
+    public FlagTypeScriptable GetMyType()
+    {
+        if (OverrideBy != null)
+        {
+            return OverrideBy;
+        }
+        
+        return this;
+    }
 }
 
 //飛兵要用新的scriptable, 還是統一clip名稱??? (baseClipName)
