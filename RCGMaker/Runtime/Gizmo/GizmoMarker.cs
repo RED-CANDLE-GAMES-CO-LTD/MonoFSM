c335d2f191ef8cbef0912cb1f18315e15e7a800e
huw12313212 <huw12313212@gmail.com>
2024-05-05 17:11:23 +0800

    Fix Compile error

--- RCGMaker/Runtime/Gizmo/GizmoMarker.cs
+++ RCGMaker/Runtime/Gizmo/GizmoMarker.cs
@@ -1,7 +1,12 @@
-﻿using UnityEditor;
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using UnityEngine;
 
+
+#if UNITY_EDITOR
 [CanEditMultipleObjects]
+#endif
 public class GizmoMarker : MonoBehaviour, IDrawHierarchyBackGround
 {
 #if UNITY_EDITOR
