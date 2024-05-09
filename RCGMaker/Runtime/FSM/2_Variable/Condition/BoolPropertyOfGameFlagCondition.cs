8290caae8e859b12a7e96212e1d801397cc0955d
詹易衡 <jerryee@redcandlegames.com>
2024-05-09 22:14:20 +0800

    fix

--- RCGMaker/Runtime/FSM/2_Variable/Condition/BoolPropertyOfGameFlagCondition.cs
+++ RCGMaker/Runtime/FSM/2_Variable/Condition/BoolPropertyOfGameFlagCondition.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace RCGMaker.Runtime.FSM._2_Variable.Condition
@@ -5,8 +6,19 @@ namespace RCGMaker.Runtime.FSM._2_Variable.Condition
     //選到一個GameFlagBase的bool property
     public class BoolPropertyOfGameFlagCondition : AbstractFieldConditionComp<bool, GameFlagBase>
     {
-        protected override bool isValid =>
-            (bool)sourceObject.GetType().GetProperty(propertyName).GetValue(sourceObject) == TargetValue;
+        protected override bool isValid => SourceValue == TargetValue;
+        // (bool)sourceObject.GetType().GetProperty(propertyName).GetValue(sourceObject) == TargetValue;
+
+        // private delegate bool GetBoolProperty(GameFlagBase sourceObject);
+        //
+        // private GetBoolProperty getBoolProperty;
+        //
+        // private void Awake()
+        // {
+        //     getBoolProperty = (GetBoolProperty)Delegate.CreateDelegate(typeof(GetBoolProperty),
+        //         sourceObject, propertyName);
+        //     
+        // }
 
         protected override string nameDescription =>
             $"[Condition] Flag:{sourceObject.name} {propertyName} == {TargetValue}";
