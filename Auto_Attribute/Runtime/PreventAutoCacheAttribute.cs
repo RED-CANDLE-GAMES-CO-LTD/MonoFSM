cca0daeebe57e02b12e4cae169b87b1316374d7e
Jerryee <jerryee@redcandlegames.com>
2024-08-20 15:16:27 +0800

    memo

--- Auto_Attribute/Runtime/PreventAutoCacheAttribute.cs
+++ Auto_Attribute/Runtime/PreventAutoCacheAttribute.cs
@@ -2,6 +2,7 @@ using System;
 
 namespace Auto_Attribute.Runtime
 {
+    //Editor Only會比較好記?
     [AttributeUsage(AttributeTargets.Field)]
     public class PreventAutoCacheAttribute : Attribute
     {
