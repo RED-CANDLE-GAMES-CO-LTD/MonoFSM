65ba68e0ea295321ce07f7746e20ad8f6bf365fa
Jerryee <jerryee@redcandlegames.com>
2024-08-27 19:58:49 +0800

    fix lateUpdate

--- RCGMaker/Runtime/Entity/UpdateLoopManager.cs
+++ RCGMaker/Runtime/Entity/UpdateLoopManager.cs
@@ -5,29 +5,72 @@ using UnityEngine.Profiling;
 
 namespace RCGMaker.Core
 {
-    public interface IProxyUpdate
+    public interface IProxyUpdate : ICustomUpdate
     {
-        GameObject gameObject { get; }
         void UpdateProxy();
     }
 
-    public interface IProxyLateUpdate
+    public interface IProxyLateUpdate : ICustomUpdate
     {
-        GameObject gameObject { get; }
         void LateUpdateProxy();
     }
 
+    public interface ICustomUpdate
+    {
+        GameObject gameObject { get; }
+    }
+
+    // public class UpdateList<T> where T : ICustomUpdate
+    // {
+    //     public UpdateList(delegate updateFunc)
+    //     {
+    //         
+    //     }
+    //     private HashSet<T> _updateSet = new();
+    //     private List<T> updateList = new();
+    //     private List<T> toUnregisterUpdateList = new();
+    //
+    //     public void RegisterUpdate(T updateTarget)
+    //     {
+    //         updateList.Add(updateTarget);
+    //     }
+    //
+    //     public void UnregisterUpdate(T updateTarget)
+    //     {
+    //         toUnregisterUpdateList.Add(updateTarget);
+    //     }
+    //
+    //     public void Update()
+    //     {
+    //         foreach (var updateTarget in updateList)
+    //         {
+    //             _updateSet.Add(updateTarget);
+    //         }
+    //
+    //         foreach (var updateTarget in toUnregisterUpdateList)
+    //         {
+    //             _updateSet.Remove(updateTarget);
+    //         }
+    //
+    //         toUnregisterUpdateList.Clear();
+    //         updateList.Clear();
+    //         foreach (var updateTarget in _updateSet)
+    //         {
+    //             Profiler.BeginSample("updateTarget", updateTarget.gameObject);
+    //             updateTarget.UpdateProxy();
+    //             Profiler.EndSample();
+    //         }
+    //     }
+    // }
+
     public class UpdateLoopManager : SingletonBehaviour<UpdateLoopManager>, IClearReference
     {
+        private HashSet<IProxyUpdate> _updateSet = new();
         private List<IProxyUpdate> updateList = new();
-
         private List<IProxyUpdate> toUnregisterUpdateList = new();
         
-        private List<IProxyLateUpdate> lateUpdateList = new();
-
-        private HashSet<IProxyUpdate> _updateSet = new();
         private HashSet<IProxyLateUpdate> _lateUpdateSet = new();
-
+        private List<IProxyLateUpdate> lateUpdateList = new();
         private List<IProxyLateUpdate> toUnregisterLateUpdateList = new();
 
         
@@ -38,17 +81,20 @@ namespace RCGMaker.Core
             // updateList.Add(updateTarget);
         }
 
+
+        public void UnregisterUpdate(IProxyUpdate updateTarget)
+        {
+            
+            // updateList.Remove(updateTarget);
+            toUnregisterUpdateList.Add(updateTarget);
+        }
+
         public void RegisterLateUpdate(IProxyLateUpdate updateTarget)
         {
             // toUnregisterLateUpdateList.Add(updateTarget);
             lateUpdateList.Add(updateTarget);
         }
 
-        public void UnregisterUpdate(IProxyUpdate updateTarget)
-        {
-            // updateList.Remove(updateTarget);
-            toUnregisterUpdateList.Add(updateTarget);
-        }
 
         public void UnregisterLateUpdate(IProxyLateUpdate updateTarget)
         {
@@ -94,7 +140,7 @@ namespace RCGMaker.Core
             lateUpdateList.Clear();
             toUnregisterLateUpdateList.Clear();
 
-            foreach (var lateUpdateTarget in toUnregisterLateUpdateList)
+            foreach (var lateUpdateTarget in _lateUpdateSet)
             {
                 Profiler.BeginSample("lateUpdateTarget", lateUpdateTarget.gameObject);
                 lateUpdateTarget.LateUpdateProxy();
