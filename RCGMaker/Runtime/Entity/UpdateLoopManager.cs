5f566d436bca9a58daa6b430d47941c862505ad6
Jerryee <jerryee@redcandlegames.com>
2024-08-27 19:20:55 +0800

    fix update loop manager

--- RCGMaker/Runtime/Entity/UpdateLoopManager.cs
+++ RCGMaker/Runtime/Entity/UpdateLoopManager.cs
@@ -20,31 +20,58 @@ namespace RCGMaker.Core
     public class UpdateLoopManager : SingletonBehaviour<UpdateLoopManager>, IClearReference
     {
         private List<IProxyUpdate> updateList = new();
+
+        private List<IProxyUpdate> toUnregisterUpdateList = new();
+        
         private List<IProxyLateUpdate> lateUpdateList = new();
 
+        private HashSet<IProxyUpdate> _updateSet = new();
+        private HashSet<IProxyLateUpdate> _lateUpdateSet = new();
+
+        private List<IProxyLateUpdate> toUnregisterLateUpdateList = new();
+
+        
         public void RegisterUpdate(IProxyUpdate updateTarget)
         {
             updateList.Add(updateTarget);
+            // toUnregisterUpdateList.Add(updateTarget);
+            // updateList.Add(updateTarget);
         }
 
         public void RegisterLateUpdate(IProxyLateUpdate updateTarget)
         {
+            // toUnregisterLateUpdateList.Add(updateTarget);
             lateUpdateList.Add(updateTarget);
         }
 
         public void UnregisterUpdate(IProxyUpdate updateTarget)
         {
-            updateList.Remove(updateTarget);
+            // updateList.Remove(updateTarget);
+            toUnregisterUpdateList.Add(updateTarget);
         }
 
         public void UnregisterLateUpdate(IProxyLateUpdate updateTarget)
         {
-            lateUpdateList.Remove(updateTarget);
+            // lateUpdateList.Remove(updateTarget);
+            toUnregisterLateUpdateList.Add(updateTarget);
         }
 
         private void Update()
         {
             foreach (var updateTarget in updateList)
+            {
+                _updateSet.Add(updateTarget);
+            }
+
+            foreach (var updateTarget in toUnregisterUpdateList)
+            {
+                _updateSet.Remove(updateTarget);
+            }
+
+            toUnregisterUpdateList.Clear();
+            updateList.Clear();
+
+            foreach (var updateTarget in _updateSet)
             {
                 Profiler.BeginSample("updateTarget", updateTarget.gameObject);
                 updateTarget.UpdateProxy();
@@ -55,6 +82,19 @@ namespace RCGMaker.Core
         private void LateUpdate()
         {
             foreach (var lateUpdateTarget in lateUpdateList)
+            {
+                _lateUpdateSet.Add(lateUpdateTarget);
+            }
+
+            foreach (var lateUpdateTarget in toUnregisterLateUpdateList)
+            {
+                _lateUpdateSet.Remove(lateUpdateTarget);
+            }
+
+            lateUpdateList.Clear();
+            toUnregisterLateUpdateList.Clear();
+
+            foreach (var lateUpdateTarget in toUnregisterLateUpdateList)
             {
                 Profiler.BeginSample("lateUpdateTarget", lateUpdateTarget.gameObject);
                 lateUpdateTarget.LateUpdateProxy();
