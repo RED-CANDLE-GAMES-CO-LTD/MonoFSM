94d8dd9f75ed066491732a4841fe615fdd7765cb
Jerryee <jerryee@redcandlegames.com>
2024-09-06 19:18:52 +0800

    rm null

--- RCGMaker/Runtime/LifeCycle/DestroyAllGameObjects.cs
+++ RCGMaker/Runtime/LifeCycle/DestroyAllGameObjects.cs
@@ -36,6 +36,7 @@ public static class RCGLifeCycle
                 // gObject.gameObject.name += " (RCGLifeCycle)";
             }
         }
+        
     }
     private static readonly List<GameObject> DontDestroyObjList = new();
 
