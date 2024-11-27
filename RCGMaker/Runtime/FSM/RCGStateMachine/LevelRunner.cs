2364ea91658af501caaa4de6aab8305cb22eea45
Jerryee <jerryee@redcandlegames.com>
2024-11-27 17:48:20 +0800

    action

--- RCGMaker/Runtime/FSM/RCGStateMachine/LevelRunner.cs
+++ RCGMaker/Runtime/FSM/RCGStateMachine/LevelRunner.cs
@@ -1,11 +1,30 @@
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace RCGMaker.Core
 {
     [DefaultExecutionOrder(10000)]
-    public class LevelRunner : MonoBehaviour
+    public class LevelRunner : SingletonBehaviour<LevelRunner>
     {
+        //FIXME: 
+        [MenuItem("RCG/ResetLevel %R")]
+        public static void TestResetLevel()
+        {
+            Debug.Log("ResetLevel CMD+Shift+R");
+            Instance.ResetLevel();
+        }
+        
+        public void ResetLevel()
+        {
+            //每次重置都要做的, LevelReset, LevelResetAfter?
+            PoolManager.LevelResetChildrenPrepareRuntimeData(level);
+            //大便！
+            PoolManager.HandleEnterLevelReset(level);
+            //FIXME: 再重整一下
+            PoolManager.LevelResetStart(level);
+        }
+        
         // [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         // public static void Init()
         // {
@@ -14,13 +33,14 @@ namespace RCGMaker.Core
         // }
         //
         // private static void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
+        private GameObject level;
         void Start()
         {
             // Debug.Log("OnSceneLoaded" + arg0.name);
             var arg0 = gameObject.scene;
             // var level = new GameObject("Level");
             var allObjs = arg0.GetRootGameObjects();
-            var level = gameObject;
+            level = gameObject;
             //put all objects into level
             foreach (var obj in allObjs)
             {
