72f90d333c428c5042aa117c0a6f5562f8c6bf0e
Jerryee <jerryee@redcandlegames.com>
2025-04-09 14:46:10 +0800

    namespace

--- RCGMakerFSMCore/Runtime/2_Variable/VariableModifier/VariableFloatBoundModifier.cs
+++ RCGMakerFSMCore/Runtime/2_Variable/VariableModifier/VariableFloatBoundModifier.cs
@@ -8,82 +8,85 @@ using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Events;
 
-
-//get operation?
-public interface IVariableFloatOperation // //乘區  好像不是variable, 應該是 Effect的FinalValue Calculation
+namespace MonoFSM.Variable
 {
-    float ApplyOperation(float value);
-}
-
-public interface IVariableFloatSetOperation //很確定是set variable時的operation
-{
-    float SetOperation(float value);
-}
-
-public interface AbstractVariableModifier<T>
-{
-    T BeforeSetValueModifyCheck(T value);
-    T AfterGetValueModifyCheck(T value);
-}
-
-
-//限制VariableFloat的最小最大值，可以用RCGEventSender倒接事件
-public class VariableFloatBoundModifier : MonoBehaviour, AbstractVariableModifier<float>
-{
-    [PreviewInInspector] [AutoParent] VarFloat _monoVar;
-
-    // [Auto] VariableFloat variable;
-    // [HideIf(nameof(MinVar))] public float min = 0;
-    //
-    // [HideIf(nameof(MaxVar))] public float max = 1;
-    //
-    // //ex: 血量
-    // //這會不會很麻煩每次都要設定？
-    //
-    // [DropDownRef] [SerializeField] VarFloat MinVar;
-    // [DropDownRef] [SerializeField] VarFloat MaxVar; //好像應該用繼承的
-    
-    //FIXME: simple bound怎麼設計？
-    [Component] [AutoChildren] IFloatProvider[] _floatProviderArray = Array.Empty<IFloatProvider>();
-    [PreviewInInspector] [Component] IFloatProvider _minValueProvider => _floatProviderArray.Length > 0 ? _floatProviderArray[0] : null;
-    [PreviewInInspector] [Component] IFloatProvider _maxValueProvider => _floatProviderArray.Length > 1 ? _floatProviderArray[1] : null;
-
-    [ShowInInspector]
-    public float MinValue =>
-        _minValueProvider?.Value ?? Mathf.NegativeInfinity; //MaxVar != null ? MaxVar.CurrentValue : max;
-
-    [ShowInInspector]
-    public float MaxValue => _maxValueProvider?.Value ?? Mathf.Infinity; //MinVar != null ? MinVar.CurrentValue : min;
-
-    public float Percentage => (_monoVar.CurrentValue - MinValue) / (MaxValue - MinValue);
-
-    public UnityEvent OnMin;
-    public UnityEvent OnMax;
-
-    public float SetOperation(float value)
+        
+    //get operation?
+    public interface IVariableFloatOperation // //乘區  好像不是variable, 應該是 Effect的FinalValue Calculation
     {
-        if (value < MinValue)
-        {
-            value = MinValue;
-            OnMin?.Invoke();
-        }
-
-        if (value > MaxValue)
-        {
-            value = MaxValue;
-            OnMax?.Invoke();
-        }
-
-        return value;
+        float ApplyOperation(float value);
     }
 
-    public float BeforeSetValueModifyCheck(float value)
+    public interface IVariableFloatSetOperation //很確定是set variable時的operation
     {
-        return SetOperation(value);
+        float SetOperation(float value);
     }
 
-    public float AfterGetValueModifyCheck(float value)
+    public interface AbstractVariableModifier<T>
     {
-        return value; //要再bound一次嗎？
+        T BeforeSetValueModifyCheck(T value);
+        T AfterGetValueModifyCheck(T value);
+    }
+
+
+    //限制VariableFloat的最小最大值，可以用RCGEventSender倒接事件
+    public class VariableFloatBoundModifier : MonoBehaviour, AbstractVariableModifier<float>
+    {
+        [PreviewInInspector] [AutoParent] VarFloat _monoVar;
+
+        // [Auto] VariableFloat variable;
+        // [HideIf(nameof(MinVar))] public float min = 0;
+        //
+        // [HideIf(nameof(MaxVar))] public float max = 1;
+        //
+        // //ex: 血量
+        // //這會不會很麻煩每次都要設定？
+        //
+        // [DropDownRef] [SerializeField] VarFloat MinVar;
+        // [DropDownRef] [SerializeField] VarFloat MaxVar; //好像應該用繼承的
+        
+        //FIXME: simple bound怎麼設計？
+        [Component] [AutoChildren] IFloatProvider[] _floatProviderArray = Array.Empty<IFloatProvider>();
+        [PreviewInInspector] [Component] IFloatProvider _minValueProvider => _floatProviderArray.Length > 0 ? _floatProviderArray[0] : null;
+        [PreviewInInspector] [Component] IFloatProvider _maxValueProvider => _floatProviderArray.Length > 1 ? _floatProviderArray[1] : null;
+
+        [ShowInInspector]
+        public float MinValue =>
+            _minValueProvider?.Value ?? Mathf.NegativeInfinity; //MaxVar != null ? MaxVar.CurrentValue : max;
+
+        [ShowInInspector]
+        public float MaxValue => _maxValueProvider?.Value ?? Mathf.Infinity; //MinVar != null ? MinVar.CurrentValue : min;
+
+        public float Percentage => (_monoVar.CurrentValue - MinValue) / (MaxValue - MinValue);
+
+        public UnityEvent OnMin;
+        public UnityEvent OnMax;
+
+        public float SetOperation(float value)
+        {
+            if (value < MinValue)
+            {
+                value = MinValue;
+                OnMin?.Invoke();
+            }
+
+            if (value > MaxValue)
+            {
+                value = MaxValue;
+                OnMax?.Invoke();
+            }
+
+            return value;
+        }
+
+        public float BeforeSetValueModifyCheck(float value)
+        {
+            return SetOperation(value);
+        }
+
+        public float AfterGetValueModifyCheck(float value)
+        {
+            return value; //要再bound一次嗎？
+        }
     }
 }
