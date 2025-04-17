bc7f2f269fbb708102fc8ad2be756ef4a34915cf
huw12313212 <huw12313212@gmail.com>
2025-04-17 16:16:53 +0800

    Fix

--- MonoFSM_Core/Runtime/2_Variable/Condition/VariableFloatValueCondition.cs
+++ MonoFSM_Core/Runtime/2_Variable/Condition/VariableFloatValueCondition.cs
@@ -19,30 +19,36 @@ namespace MonoFSM.Variable.Condition
     /// <summary>
     /// 和FloatCompareCondition重複？還是這個要做成簡單版？
     /// </summary>
-    public class VariableFloatValueCondition : AbstractConditionComp
+    public class VariableFloatValueCondition : AbstractConditionComp,IResetStart,ITransitionCheckInvoker
     {
-        protected override string Description => _monoVarFloatProvider != null
-            ? name = "[Condition] " + _monoVarFloatProvider.Description + " " + op + " " + targetValue
+        protected override string Description => _monoVariableFloat != null
+            ? name = "[Condition] " + _monoVariableFloat + " " + op + " " + targetValue
             : name = "[Condition]";
 
-        // [Obsolete] [DropDownRef] public VarFloat _monoVarFloat;
+        private void OnVariableChanged()
+        {
+            Rename();
+        }
 
-        public IFloatProvider _monoVarFloatProvider;
-        [DropDownRef]
-        public VarFloat _monoVarFloat;
+        // [DropDownRef]
+        // public VarFloat _monoVarFloat;
         public Operator op;
-        public float targetValue;
-
+        
+        
+        [OnValueChanged(nameof(OnVariableChanged))] [FormerlySerializedAs("variableBool")] [Required] [DropDownRef]
+        // [ValueDropdown(nameof(GetBoolVariables))]
+        public VarFloat _monoVariableFloat;
+        //FIXME: 要用VarBoolProvider?
+        // [Component] [Auto] public VariablefloatProviderRef _varFloatProvider;
+        // [Component] [Auto] IBoolProvider _boolValue; //會再度抓到自己，...沒屁用
+        public float targetValue = 0;
+        //FIXME: 會有需求要比對其他東西嗎？
         protected override bool IsValid
         {
             get
             {
-                // if (_monoVarFloat == null)
-                // {
-                //     // Debug.LogError("variableFloat is null", this);
-                //     return false;
-                // }
-                var value = _monoVarFloatProvider.Value;
+                
+                var value = _monoVariableFloat.Value;
 
                 switch (op)
                 {
@@ -61,7 +67,25 @@ namespace MonoFSM.Variable.Condition
                 }
 
                 return false;
-            }
+            } 
+        }
+
+        //FIXME: condition本來就要實作狀態變化？必須listener? 會不會太強求？
+        public void OnValueChanged(float value)
+        {
+            if (_parentConditionChangeListener == null)
+                // Debug.LogError("VarBoolValueCondition: No parent transition found", this);
+                return;
+            _parentConditionChangeListener.OnConditionChanged();
+        }
+
+        public void ResetStart()
+        {
+            Debug.Log("LevelResetPrepareRuntimeData", this);
+            if (_parentConditionChangeListener == null)
+                return;
+            _monoVariableFloat.Field.AddListener(OnValueChanged, this);
+            //需要清掉嗎？還是leveldestroy就會自己把field的listener清掉？
         }
     }
 }
