d755a803573badcede968351194adf523a7c2d51
Jerryee <jerryee@redcandlegames.com>
2025-05-12 22:58:29 +0800

    close action

---
+++ MonoFSM_Core/Runtime/PoolObject/Action/StopFSMAction.cs
@@ -0,0 +1,14 @@
+using MonoFSM_Core.Runtime.Action;
+
+namespace RCGMaker.Runtime.ObjectPool
+{
+    //把FSM關掉，不一定是要回pool?
+    public class StopFSMAction: AbstractStateAction
+    {
+        [AutoParent] private StateMachineOwner _owner;
+        protected override void OnStateEnterImplement()
+        {
+            _owner.gameObject.SetActive(false);
+        }
+    }
+}
