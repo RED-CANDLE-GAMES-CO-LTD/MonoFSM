900712c5de479ebf03c386ab67a6bc5a51a1c309
huw12313212 <huw12313212@gmail.com>
2025-06-12 17:07:28 +0800

    整理sceneConnection

--- MonoFSM_Core/Runtime/3_FlagData/SceneConnection.cs
+++ MonoFSM_Core/Runtime/3_FlagData/SceneConnection.cs
@@ -1,15 +1,31 @@
 using System;
 using RCGMaker.Core;
+using Sirenix.OdinInspector;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using UnityEngine;
+using Object = System.Object;
 
 public class SceneConnection : MonoBehaviour,IOnBuildSceneSavingCallbackReceiver
 {
 
     public string ConnectionGUID => this.gameObject.TryGetCompOrAdd<GuidComponent>().Guid.ToString();
 
+
     public SceneConnectionData connectionData;
     
     
+#if UNITY_EDITOR
+    public string sceneGUID = "";
+    public string GetSceneGUID()
+    {
+        return AssetDatabase.AssetPathToGUID(this.gameObject.scene.path);
+    }
+#endif
+
+
     public ConnectionRegisteredEntry FindDestinationEntry () => connectionData.FindConnectionDestinationData(this);
 
     public bool IsOnTransition =>  connectionData.IsTransitioning();
@@ -20,9 +36,27 @@ public class SceneConnection : MonoBehaviour,IOnBuildSceneSavingCallbackReceiver
 
     private void OnValidate()
     {
-        if (connectionData.IsValideBind(this) == false)
+        #if UNITY_EDITOR
+        if (sceneGUID == "")
+        {
+            sceneGUID = GetSceneGUID();
+            EditorUtility.SetDirty(this);
+            
+        }
+        
+        if (sceneGUID != GetSceneGUID())
         {
             connectionData = null;
+            sceneGUID = GetSceneGUID();
+            EditorUtility.SetDirty(this);
+            return;
+        }
+        #endif
+        
+        if (connectionData!=null && connectionData.IsValideBind(this) == false)
+        {
+            connectionData = null;
+            
         }
     }
 }
