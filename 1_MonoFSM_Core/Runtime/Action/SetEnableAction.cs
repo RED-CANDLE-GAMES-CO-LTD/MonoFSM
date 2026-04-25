b0135fbc81939fbad6d6e36abf11dcd90145f03e
Jerryee <jerryee@redcandlegames.com>
2026-04-25 18:12:07 +0800

    enable action (不太好...)

--- 1_MonoFSM_Core/Runtime/Action/SetEnableAction.cs
+++ 1_MonoFSM_Core/Runtime/Action/SetEnableAction.cs
@@ -1,24 +1,28 @@
 using MonoFSM.Core.Runtime.Action;
+using MonoFSM.Variable;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace _1_MonoFSM_Core.Runtime.Action
 {
+    //FIXME: 不好...會不好回查 reference?
     public class SetEnableAction : AbstractStateAction
     {
         public override string Description => "Set Enable: " + _component.GetType().Name + " to " +
-                                              (_isToggle ? "Toggle" : _enable.ToString());
+                                              (_isToggle ? "Toggle" : _enable.Value.ToString());
         public Behaviour _component;
         public Behaviour[] _addcomponents;
         public bool _isToggle;
-        [HideIf(nameof(_isToggle), false)] public bool _enable;
+
+        [HideIf(nameof(_isToggle))] [SerializeField]
+        private VarBoolWrapper _enable;
 
         protected override void OnActionExecuteImplement()
         {
             if (_isToggle)
                 _component.enabled = !_component.enabled;
             else
-                _component.enabled = _enable;
+                _component.enabled = _enable.Value;
 
             foreach (var comp in _addcomponents)
             {
---
+++ 1_MonoFSM_Core/Runtime/Action/SetGameObjectActiveAction.cs
@@ -0,0 +1,34 @@
+using MonoFSM.Core.Runtime.Action;
+using MonoFSM.Variable;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace _1_MonoFSM_Core.Runtime.Action
+{
+    public class SetGameObjectActiveAction : AbstractStateAction
+    {
+        public override string Description => "SetActive: " + (_target != null ? _target.name : "null") + " to " +
+                                              (_isToggle ? "Toggle" : _active.Value.ToString());
+
+        public GameObject _target;
+        public GameObject[] _addTargets;
+        public bool _isToggle;
+        [HideIf(nameof(_isToggle))]
+        [SerializeField] private VarBoolWrapper _active;
+
+        protected override void OnActionExecuteImplement()
+        {
+            bool value;
+            if (_isToggle)
+                value = !_target.activeSelf;
+            else
+                value = _active.Value;
+
+            _target.SetActive(value);
+            foreach (var go in _addTargets)
+            {
+                go.SetActive(value);
+            }
+        }
+    }
+}
